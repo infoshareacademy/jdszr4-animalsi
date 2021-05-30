fd72894088f0da8c940eec6e2c0abefaff7bef15
Dawid <tedi29571@gmail.com>
2021-05-30 11:57:23 +0200

    poprawka stylu

--- wyczyszczone_tabelki.sql
+++ wyczyszczone_tabelki.sql
@@ -1,29 +1,29 @@
 -- czyszczenie danych, ktore zostaly uznane jako anomalia
 
-create table tab_rental_clean as
-    select 
+CREATE TABLE tab_rental_clean AS
+    SELECT 
       rental_id,
       rental_date,
       inventory_id,
       customer_id,
       return_date 
-    from rental r2 
-    where rental_id  not in (1,291) 
+    FROM rental r2 
+    WHERE rental_id  NOT IN (1,291) 
 
-create table tab_payment_clean as
-    select 
+CREATE TABLE tab_payment_clean as
+    SELECT 
       p.payment_id,
       rental_id,
       amount,
       payment_date 
-    from payment p 
-    where amount >0 
-      and rental_id  not in (1,291)
+    FROM payment p 
+    WHERE amount >0 
+      AND rental_id  NOT IN (1,291)
 
 
-create table tab_inventory_clean as
-    select
+CREATE TABLE tab_inventory_clean AS
+    SELECT
       inventory_id, 
       film_id
-    from inventory i 
-    where i.film_id <> 257
+    FROM inventory i 
+    WHERE i.film_id <> 257
