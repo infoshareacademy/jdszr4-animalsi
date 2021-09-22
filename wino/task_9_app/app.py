382736b2a3007cfa78baa8b302b82169714f06c3
Dawid <tedi29571@gmail.com>
2021-09-22 19:51:22 +0200

    app version with 3 defaults parameters

--- wino/task_9_app/app.py
+++ wino/task_9_app/app.py
@@ -1,12 +1,14 @@
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 from flask import Flask, request, jsonify, render_template
 import pickle
 
 #wczytuje classe modelu flask
 app = Flask(__name__)
 
-#wczytuje nasz zapiklowany model
-model = pickle.load(open('model_final_rf.pkl', 'rb'))
+#wczytuje nasz zapiklowany model i scaler
+model = pickle.load(open('model_final_xgb.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 #pobiera nasz template strony
 @app.route('/')
@@ -19,16 +21,23 @@ def predict():
 
     int_features = [float(x) for x in request.form.values()]
     final_features1 = [np.array(int_features)]
-    #definicja domyslnych ustawien modelu
-    defaults = [np.array([4,2])]
-    final_features = np.hstack([final_features1, defaults])
-    prediction = model.predict(final_features)
+    #definicja domyslnych ustawien modelu, dla parametru 4,5 i 8 
+    # oraz podzielenie zbioru tak zeby wcisnac te domyslne wartosci parametrow pomiedzy wprowadzone dane 
+    # i doprowadzic do wymaganej postaci dla predykcji
+    a = final_features1[0][0:4]
+    defaults_4_5 = np.array([0.06,13])
+    b = final_features1[0][4:6]
+    defaults_8 = np.array([3.31])
+    c = final_features1[0][6:]
 
-    output = ["poor, don't drink it. Life is too short" if prediction[0] == 1 else "good, i recommend"]
+    #laczenie w zbior do predykcji
+    final_features = [np.hstack([a,defaults_4_5,b, defaults_8,c ])]
+    prediction = model.predict(scaler.transform(final_features))
 
-    return render_template('index.html', prediction_text="Your wine is  {}".format(output).replace('["', '').replace('"]',''))
+    output = ["poor, not drink it. Life is too short" if prediction[0] == 1 else "good, i recommend"]
 
-#w tym miejscu dokonujemy predykcji chyba na domyslnych danych ??
+    return render_template('index.html', prediction_text="Your wine is  {}".format(output).replace("['", "").replace("']",""))
+
 @app.route('/results',methods=['POST'])
 def results():
 
--- wino/task_9_app/request.py
+++ wino/task_9_app/request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/results'
-r = requests.post(url,json={'alcohol':5, 'sulphates':200, 'ph':400})
+r = requests.post(url,json={'chlorides':0.079, 'free sulfur dioxide':14, 'ph':3:31})
 
 print(r.json())
